21eb9aef3cd164a79c33bdb327ef35f336e044cd
Andreas Wissel <business@andreaswissel.com>
2023-03-23 09:15:28 +0100

    lab-4

--- design-systems/src/app/button/button.component.ts
+++ design-systems/src/app/button/button.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export type ButtonType = 'primary' | 'secondary';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() public label: string = 'Button Label';
+  @Input() public type: ButtonType = 'primary';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
